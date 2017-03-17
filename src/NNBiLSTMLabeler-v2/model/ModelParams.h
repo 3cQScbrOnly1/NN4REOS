9fb5459d60a49ab88bc2636134c8e7748ad92339
YuNan <510727367@qq.com>
2017-03-17 16:53:01 +0800

    bilstmv2

---
+++ src/NNBiLSTMLabeler-v2/model/ModelParams.h
@@ -0,0 +1,68 @@
+#ifndef SRC_ModelParams_H_
+#define SRC_ModelParams_H_
+#include "HyperParams.h"
+
+// Each model consists of two parts, building neural graph and defining output losses.
+class ModelParams{
+
+public:
+	Alphabet wordAlpha; // should be initialized outside
+	LookupTable words; // should be initialized outside
+	Alphabet extWordAlpha; // should be initialized outside
+	LookupTable extWords; // should be initialized outside
+	LSTMParams lstm_left_project;
+	LSTMParams lstm_right_project;
+	BiParams bi_lstm_project;
+	UniParams olayer_linear; // output
+public:
+	Alphabet labelAlpha; // should be initialized outside
+	SoftMaxLoss loss;
+
+
+public:
+	bool initial(HyperParams& opts, AlignedMemoryPool* mem = NULL){
+
+		// some model parameters should be initialized outside
+		if (words.nVSize <= 0 || labelAlpha.size() <= 0){
+			return false;
+		}
+		opts.wordDim = words.nDim;
+		opts.extWordDim = extWords.nDim;
+		opts.labelSize = labelAlpha.size();
+		lstm_left_project.initial(opts.rnnHiddenSize, opts.wordDim + opts.extWordDim, mem);
+		lstm_right_project.initial(opts.rnnHiddenSize, opts.wordDim + opts.extWordDim, mem);
+		bi_lstm_project.initial(opts.biRNNHiddenSize, opts.rnnHiddenSize, opts.rnnHiddenSize, true, mem);
+		opts.inputSize = opts.rnnHiddenSize * 2 * 3;
+		olayer_linear.initial(opts.labelSize, opts.inputSize, false, mem);
+		return true;
+	}
+
+
+	void exportModelParams(ModelUpdate& ada){
+		words.exportAdaParams(ada);
+		extWords.exportAdaParams(ada);
+		lstm_left_project.exportAdaParams(ada);
+		lstm_right_project.exportAdaParams(ada);
+		bi_lstm_project.exportAdaParams(ada);
+		olayer_linear.exportAdaParams(ada);
+	}
+
+
+	void exportCheckGradParams(CheckGrad& checkgrad){
+		checkgrad.add(&words.E, "words.E");
+		checkgrad.add(&(olayer_linear.W), "olayer_linear.W");
+		//checkgrad.add(&(olayer_linear.b), "olayer_linear.b");
+	}
+
+	// will add it later
+	void saveModel(){
+
+	}
+
+	void loadModel(const string& inFile){
+
+	}
+
+};
+
+#endif /* SRC_ModelParams_H_ */
